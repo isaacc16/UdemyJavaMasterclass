daa48c437d43129a490bd5517a5d0e6467b741f6
Isaac Cheng <101915396+isaacc16@users.noreply.github.com>
2023-05-16 21:06:52 +0800

    Update PositiveNegativeZero.java
    Quick Change

--- CodingExercise1_PositiveNegativeZero/PositiveNegativeZero.java
+++ CodingExercise1_PositiveNegativeZero/PositiveNegativeZero.java
@@ -2,9 +2,9 @@ public class PositiveNegativeZero {
     
     public static void checkNumber(int number) {
         
-        if (number > 0){
+        if (number > 0) {
             System.out.println("positive");
-        } else if (number< 0) {
+        } else if (number < 0) {
             System.out.println("negative");
         } else {
             System.out.println("zero");
